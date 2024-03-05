90a7cbb5bd730d1e6d6c5d1cf25d1617af526e3d
Tomsooon <160400510+Tomsooon@users.noreply.github.com>
2024-03-05 23:23:35 +0100

    Add files via upload

---
+++ src/main/java/org/example/Wisielec.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import java.sql.Array;
+import java.util.*;
+
+
+public class Wisielec {
+
+   static List<String> words = List.of("rower", "samochód", "łódź", "samolot");
+
+
+    static char[] userword;
+    static String word;
+    public static void play() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Random random = new Random();
+
+        word = words.get(random.nextInt(words.size()));
+        userword = new char[word.length()];
+
+        Arrays.fill(userword, '_');
+        System.out.println(userword);
+
+        while (!gameEnded()) {
+            System.out.println(userword);
+            System.out.println();
+            System.out.println("Podaj kolejną literę: ");
+
+            char letter = scanner.nextLine().charAt(0);
+
+            checkLetter(letter);
+        }
+
+        scanner.close();
+
+    }
+
+    private static void checkLetter(char letter) {
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == letter) {
+                userword[i] = letter;
+            }
+        }
+    }
+
+
+    private static boolean gameEnded() {
+        return false;
+
+    }
+
+
+
+}
